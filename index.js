86ecb646acff86152e383a8e3527a1d1b0d0ab71
Kasak Gupta <64642473+palindrome-kasak@users.noreply.github.com>
2024-10-29 15:53:18 +0530

    cc

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 // word vs keyword
 // var let const
 
-getName();
-console.log(getName);
-console.log(x)
-var x=7;
+// getName();
+// console.log(getName);
+// console.log(x)
+// var x=7;
 
-function getName(){
-    console.log("hello");
+// function getName(){
+//     console.log("hello");
+// }
+
+// getName();
+// console.log(getName);
+// console.log(x)
+
+// var x=1;
+// a();
+// b();
+// console.log(x)
+
+// function a(){
+//     var x=10;
+//     console.log(x);
+// }
+// function b(){
+//     var x=100;
+//     console.log(x);
+// }
+
+function a(){
+    console.log(x)
 }
-
-getName();
-console.log(getName);
-console.log(x)
